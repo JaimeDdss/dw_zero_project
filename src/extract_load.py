2353f0fabeac4e9a3f7efe11268aa2ef71f48d6b
Jaime Daniel <jjjsouza2015@gmail.com>
2024-06-27 20:31:00 -0300

    add function to create a pandas df

--- src/extract_load.py
+++ src/extract_load.py
@@ -1,6 +1,7 @@
 # Import 
 import yfinance as yf 
 from dotenv import load_dotenv
+import pandas as pd
 import os 
 
 # import das variaveis de ambiente
@@ -13,9 +14,19 @@ def data_extract(symbol, period = '2y', interval = '1d'):
     data['symbol'] = symbol
     return data
 
+def concatenar_dados(commodities):
+    todos_dados = []
+    for symbol in commodities:
+        dados = data_extract(symbol)
+        todos_dados.append(dados)
+    return pd.concat(todos_dados)
 
 
-# pegar cotacao dos ativos
+if __name__ == '__main__':
+    dados_concatenados = concatenar_dados(commodities)
+    print(dados_concatenados)
+
+
 
 # concatenar os ativos (1..2..3) -> (1)
 
